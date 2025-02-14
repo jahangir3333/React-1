ca90897c1bd008c3f0a253f327e1997392446852
jahangir3333 <mdjahangirhussain786@gmail.com>
2025-02-14 23:10:02 +0530

    publish branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 import HomePage from './HomePage'; // Import HomePage component
-
+import AboutPage from './AboutPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 function App() {
+   let style={
+    backgroundColor: 'grey',
+    padding: '10px',
+    textDecoration: 'none',
+  }
+  
   return (
-    <div className="App">
-      <HomePage /> 
-    </div>
+
+    <Router>
+      <div className="App">
+      <nav style={{display: 'flex', justifyContent: 'center'}}>
+          <Link style={style} to="/">Home</Link>
+          <br />
+          <Link style={style} to="/about">About</Link>
+      </nav>
+
+        
+        <Routes>
+          <Route exact path="/" element={<HomePage/>} />
+          <Route path="/about" element={<AboutPage/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
