1d67cf84bede49e972d2357eb597695a64f4df48
jahangir3333 <mdjahangirhussain786@gmail.com>
2025-02-15 22:52:32 +0530

    first sync

--- src/App.js
+++ src/App.js
@@ -10,29 +10,19 @@ function App() {
     textDecoration: 'none',
   }
 
-
   return (
-
-    //   <div className="App">
-    //     <nav>
-    //       <Link to="/" style={style}>Home</Link>
-    //       <Link to="/about" style={style}>About</Link>
-    //     </nav>
-
-    //   <Router>
-    //   <Routes>
-    //   <Route exact path="/" element={<HomePage/>} />
-    //   <Route path="/about" element={<AboutPage/>} />
-    //   </Routes>
-    // </Router>
-    // </div>
-
-    <div className='App'>
-    <Link to="/" style={style} element={<HomePage/>}>Home</Link>
-    <Link to="/about" style={style} element={<AboutPage/>}>Home</Link>
-
+    <div className="App">
+      <Router>
+        <nav>
+          <Link to="/" style={style}>Home</Link>
+          <Link to="/about" style={style}>About</Link>
+          </nav>
+        <Routes>
+          <Route exact path="/" element={<HomePage/>} />
+          <Route path="/about" element={<AboutPage/>} />
+        </Routes>
+      </Router>
     </div>
-
   );
 }
 
--- src/HomePage.js
+++ src/HomePage.js
@@ -3,6 +3,7 @@ import React from 'react';
 function HomePage() {
   return (
     <div id="home">
+      
       <h1>Welcome to the Home Page</h1>
       <p>This is the homepage of the application.</p>
       
